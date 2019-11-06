28903a32dd36c01b5d08f5b581ec50977bacd269
James Becker <jamsbec@gmail.com>
2019-11-06 10:02:20 +0700

    update

--- socialnet-functions/functions/index.js
+++ socialnet-functions/functions/index.js
@@ -2,14 +2,11 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
 admin.initializeApp();
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello world!");
-});
 
-exports.getScreams = functions.https.onRequest((req, res) => {
+const express = require('express');
+const app = express();
+
+app.get('/screams', (req, res) => {
   admin
     .firestore()
     .collection('screams')
@@ -24,7 +21,15 @@ exports.getScreams = functions.https.onRequest((req, res) => {
     .catch((err) => console.error(err));
 })
 
+// Best practice for API is if we have an endpoint https://baseurl.com/screams.
+// We should have a subroute for API, such as /api/. Or a subdomain, such as api.baseurl.
+
+exports.api = functions.https.onRequest(app);
+
 exports.createScream = functions.https.onRequest((req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(400).json({error: 'Method not allowed.'})
+  }
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
